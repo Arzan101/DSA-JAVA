97d3acfc665939945531680e774ce50997027d05
Arzan <khanazzu994>
2024-09-20 11:12:35 +0530

    commit

--- oops/OOPs.java
+++ oops/OOPs.java
@@ -10,9 +10,13 @@ class Student {
         System.out.println(this.rollNumber);
     }
 
-    Student(String name, int rollNumber) {
-        this.name = name;
-        this.rollNumber = rollNumber;
+    Student(Student s2) {
+        this.name = s2.name;
+        this.rollNumber = s2.rollNumber;
+
+    }
+
+    Student() {
 
     }
 }
@@ -20,8 +24,12 @@ class Student {
 public class OOPs {
 
     public static void main(String[] args) {
-        Student s1 = new Student("RAJA", 0);
-        s1.printInfo();
+        Student s1 = new Student();
+        s1.name = "John Doe";
+        s1.rollNumber = 12345;
+
+        Student s2 = new Student(s1);
+        s2.printInfo();
 
     }
 }
