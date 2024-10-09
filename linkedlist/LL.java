2e638b3c2b0264253792a1b43934cf6decdef5db
Arzan <khanazzu994>
2024-10-09 13:10:24 +0530

    commit

--- linkedlist/LL.java
+++ linkedlist/LL.java
@@ -7,11 +7,19 @@ public class LL {
     public static void main(String args[]) {
         LinkedList<String> list = new LinkedList<String>();
 
+        list.addFirst("this");
         list.addFirst("hello");
+
         System.out.print(list);
 
+        list.addLast("is");
         list.addLast("list");
         System.out.print(list);
+
+        for (int i = 0; i < list.size(); i++) {
+
+            System.out.print("->" + list.get(i));
+        }
 
     }
 }
